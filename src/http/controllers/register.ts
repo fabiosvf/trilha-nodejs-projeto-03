5dd54c6eafba2208b82b75393a5866f5bda99bba
Fabio Silva Ferreira <fabiosvf@gmail.com>
2023-12-31 20:35:46 -0300

    Implementando casos de uso - Testes e controller de autenticação

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -3,7 +3,6 @@ import { z } from 'zod';
 import { RegisterUseCase } from '@/use-cases/register';
 import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository';
 import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error';
-import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository';
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
