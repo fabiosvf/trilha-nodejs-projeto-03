ad9dfeb17b8ac2adf2fceed70fa4f1babb9897d5
Fabio Silva Ferreira <fabiosvf@gmail.com>
2024-01-17 02:29:38 -0300

    Controllers & Testes E2E - Testes E2E da autenticação

--- src/http/controllers/authenticate.spec.ts
+++ src/http/controllers/authenticate.spec.ts
@@ -1,5 +1,31 @@
-import { expect, test } from 'vitest';
+import request from 'supertest';
+import { app } from '@/app';
+import { expect, describe, it, beforeAll, afterAll } from 'vitest';
 
-test('ok', () => {
-  expect(2).toBe(2);
+describe('Authenticate (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('should be able to authenticate', async () => {
+    await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    const response = await request(app.server).post('/sessions').send({
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    expect(response.statusCode).toEqual(200);
+    expect(response.body).toEqual({
+      token: expect.any(String),
+    });
+  });
 });
