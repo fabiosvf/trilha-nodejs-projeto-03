34e83f62f93a4f56fc9f127d0e8d1c27d59a9f30
Fabio Silva Ferreira <fabiosvf@gmail.com>
2024-01-19 04:09:55 -0300

    Controllers & Testes E2E - Testes E2E de rotas de academias

---
+++ src/http/controllers/gyms/serach.spec.ts
@@ -0,0 +1,56 @@
+import request from 'supertest';
+import { app } from '@/app';
+import { expect, describe, it, beforeAll, afterAll } from 'vitest';
+import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user';
+
+describe('Search Gyms (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('should be able to search gyms by title', async () => {
+    const { token } = await createAndAuthenticateUser(app);
+
+    await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'JavaScript Gym',
+        description: 'Some description.',
+        phone: '11999999999',
+        latitude: -22.7742547,
+        longitude: -47.3503076,
+      });
+
+    await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'TypeScript Gym',
+        description: 'Some description.',
+        phone: '11999999999',
+        latitude: -22.7742547,
+        longitude: -47.3503076,
+      });
+
+    const response = await request(app.server)
+      .get('/gyms/search')
+      .set('Authorization', `Bearer ${token}`)
+      .query({
+        q: 'JavaScript',
+      })
+      .send();
+
+    expect(response.statusCode).toEqual(200);
+    expect(response.body.gyms).toHaveLength(1);
+    expect(response.body.gyms).toEqual([
+      expect.objectContaining({
+        title: 'JavaScript Gym',
+      }),
+    ]);
+  });
+});
